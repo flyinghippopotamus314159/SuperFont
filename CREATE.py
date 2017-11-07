5c75d542088c7eb6efb58fc36ea2e7ae406807a1
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-11-07 16:43:00 +0000

    Add files via upload

---
+++ CREATE.py
@@ -0,0 +1,45 @@
+""""
+SuperFont
+
+Created by FlyingHippopotamus314159 under a GNU Public Liscence v3.0 (GNU GLPv3)
+
+Commands:
+-init_prog() initalises program
+-font_init() initalises a new compressed font. font compression not currently available
+-make_chr()  make a new character
+-create_text() creates a text 
+""""
+def make_chr(font_name,chr_name,width):
+    try:
+        file=open(str("fonts/"+font_name+"//exist.f0nt"),"w")
+        file.write("y")
+    except:
+        os.makedirs(str("fonts/"+font_name))
+        file=open(str("fonts/"+font_name+"//exist.f0nt"),"w")
+        file.write("y")
+    letter=[]
+    n=0
+    writeToFile=""
+    try:
+        centreOffset=int(width)
+    except:
+        centreOffset=10
+    while n<10:
+        line=input(">")
+        line=line.center(centreOffset)
+        line=line+":"
+        writeToFile=writeToFile+line
+        n=n+1
+    file=open(str("fonts/"+font_name+str("/"+str(ord(str(chr_name)))+".f0nt")), "w")
+    file.write(writeToFile)
+    print(writeToFile)
+    file.close()
+def init_prog():
+    if not os.path.exists("fonts"):
+        os.makedirs("fonts")
+        file.close()
+    else:
+        procceed=input("Procceeding will delete all saved fonts. To procceed, press y>>>")
+        if procceed=="y":
+            shutil.rmtree("fonts", ignore_errors=False, onerror=None)
+            os.makedirs("fonts") 
